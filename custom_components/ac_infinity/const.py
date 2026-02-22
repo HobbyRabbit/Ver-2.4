d583a6653e564a48e9ca5283465facab81d329c7
HobbyRabbit <stephencewood@gmail.com>
2026-02-22 03:16:17 -0500

    Update const.py

--- custom_components/ac_infinity/const.py
+++ custom_components/ac_infinity/const.py
@@ -1,11 +1,7 @@
-"""Constants for the ac_infinity integration."""
-from bleak.exc import BleakError
-
 DOMAIN = "ac_infinity"
 
-DEVICE_TIMEOUT = 30
-UPDATE_SECONDS = 15
+CONF_MAC = "mac"
 
-BLEAK_EXCEPTIONS = (AttributeError, BleakError, TimeoutError)
+PLATFORMS = ["switch"]
 
-DEVICE_MODEL = {1: "Controller 67", 7: "Controller 69", 11: "Controller 69 Pro"}
+PORT_COUNT = 8
