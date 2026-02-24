e0f1381de6c220fa72b07cd300fe502fc6627319
HobbyRabbit <stephencewood@gmail.com>
2026-02-24 12:31:03 -0500

    Update __init__.py

--- custom_components/ac_infinity/__init__.py
+++ custom_components/ac_infinity/__init__.py
@@ -1,21 +1,36 @@
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+from .const import DOMAIN
 from .coordinator import ACInfinityCoordinator
 
-DOMAIN = "ac_infinity"
 
-async def async_setup_entry(hass, entry):
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = ACInfinityCoordinator(
         hass,
-        entry.data["mac"],
-        entry.title,
+        entry.data["mac"]
     )
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
-        ["switch"],
+        ["switch", "number"]
     )
 
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_unload_platforms(entry, ["switch", "number"])
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
